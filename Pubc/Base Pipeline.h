94b527f20f123ba89c7f93e91acb61d017bb3486
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-07 17:47:54 +0200

    Add code quality header to pipeline.h

--- Pubc/Base Pipeline.h
+++ Pubc/Base Pipeline.h
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+/*
+ *   Quality of this code: sketch
+ */
+
 #pragma once
 
 #include "HephaestusBase/Pubc/Interface Pipeline.h"
