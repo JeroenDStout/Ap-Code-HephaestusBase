0957639e0de913bdc89b681d9872af501ee98f5a
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-14 01:17:59 +0200

    Create simple version of smartcopy

--- Pubc/Pipe Tool Smartcopy.cpp
+++ Pubc/Pipe Tool Smartcopy.cpp
@@ -3,6 +3,8 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include "BlackRoot/Pubc/Threaded IO Stream.h"
+#include "BlackRoot/Pubc/Files.h"
+#include "BlackRoot/Pubc/FileSource Snooper.h"
 
 #include "HephaestusBase/Pubc/Pipe Tool.h"
 #include "HephaestusBase/Pubc/Pipe Tool Register.h"
@@ -18,11 +20,31 @@ namespace Tools {
         void Run(PipeToolInstr &) const override;
     };
     
-    void SmartCopy::Run(PipeToolInstr &) const
+    void SmartCopy::Run(PipeToolInstr & instr) const
     {
-        using cout = BlackRoot::Util::Cout;
+        using cout       = BlackRoot::Util::Cout;
+        using FileSource = BlackRoot::Util::FileSourceSnooper<BlackRoot::IO::BaseFileSource>;
+        using Time       = BlackRoot::IO::FileTime;
 
-        cout{} << "Dummy SmartCopy" << std::endl;
+            // Use FileSourceSnooper to log reads
+        FileSource fs;
+
+            // Ensure directory and copy file
+        fs.CreateDirectories(instr.FileOut.parent_path());
+        if (fs.Exists(instr.FileOut)) {
+            fs.Remove(instr.FileOut);
+        }
+        fs.CopyFile(instr.FileIn, instr.FileOut);
+
+            // Book-keep paths we used
+        for (const auto & it : fs.GetList()) {
+            if (it.WriteAccess) {
+                instr.WrittenFiles.push_back({ it.Path });
+            }
+            else {
+                instr.ReadFiles.push_back({ it.Path, it.PreviousLastWriteTime });
+            }
+        }
     }
 
     HE_PIPE_DEFINE(SmartCopy);
